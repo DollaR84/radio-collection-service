7e2f233ec7f87d223e92496fe675b10dc40f7d9b
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2025-04-28 10:15:43 +0300

    fixed offset and limit stations read

--- src/db/gateways/station/read.py
+++ src/db/gateways/station/read.py
@@ -12,18 +12,28 @@ from ...models import Station
 class GetStationGateway(BaseGateway[int, Station]):
 
     async def get_station(self, station_id: int) -> Optional[domain.StationModel]:
+        error_message = "Error get station"
+
         stmt = select(Station)
         stmt = stmt.where(Station.id == station_id)
-        error_message = "Error get station"
 
         station = await self._get(stmt, error_message)
         if station:
             return domain.StationModel(**station.dict(exclude=["id", "created_at", "updated_at"]))
         return None
 
-    async def get_stations(self) -> list[domain.StationModel]:
-        stmt = select(Station)
+    async def get_stations(
+            self,
+            offset: Optional[int] = None,
+            limit: Optional[int] = None,
+    ) -> list[domain.StationModel]:
         error_message = "Error get stations"
+        stmt = select(Station)
+
+        if offset:
+            stmt = stmt.offset(offset)
+        if limit:
+            stmt = stmt.limit(limit)
 
         stations = await self._get(stmt, error_message, is_multiple=True)
         return [
--- src/workers/tasks/parse.py
+++ src/workers/tasks/parse.py
@@ -19,18 +19,18 @@ class BaseCollectionTask(BaseTask, is_abstract=True):
     def __init__(
             self,
             parser: FromDishka[CollectionParser],
-            get_urls: FromDishka[GetStationUrls],
+            get_all_urls: FromDishka[GetStationUrls],
             creator: FromDishka[CreateStation],
     ):
         self.parser: CollectionParser = parser
-        self.get_urls: GetStationUrls = get_urls
+        self.get_all_urls: GetStationUrls = get_all_urls
         self.creator: CreateStation = creator
 
     async def execute(self) -> None:
         collection = self.parser.get_collection(self.get_name())
         data = await collection.parse()
 
-        exists_urls = await self.get_urls()
+        exists_urls = await self.get_all_urls()
         data = [collection_item for collection_item in data if collection_item.url not in exists_urls]
 
         await self.creator([Station(**item.dict()) for item in data])
