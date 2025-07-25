15c4b06e1211c8e61160eceaec9cba88df6de75e
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2025-07-25 18:35:43 +0300

    fixed parse stations from radio browser lib

--- backend/src/application/services/parsers/collections/rb.py
+++ backend/src/application/services/parsers/collections/rb.py
@@ -1,6 +1,4 @@
-﻿import json
-import logging
-import os
+﻿import logging
 from typing import Any, Optional
 
 from pyradios import RadioBrowser
@@ -17,46 +15,28 @@ class RadioBrowserCollection(BaseCollection):
         super().__init__(name, **kwargs)
 
         self.client: RadioBrowser = RadioBrowser()
-        self.need_update: bool = True
 
     def make_url(self, **kwargs: Any) -> str:
         return ""
 
-    @property
-    def file_path(self) -> str:
-        return os.path.join(self.parser.config.upload_folder, "radio_browser.json")
-
-    def _load(self) -> None:
-        data = self.client.stations()
-
-        with open(self.file_path, "w", encoding="utf-8") as json_file:
-            json.dump(data, json_file)
-
-        self.need_update = False
-
     async def load(self, url: str, offset: Optional[int] = None, limit: Optional[int] = None) -> list[CollectionData]:
         results = []
-        if self.need_update:
-            self._load()
 
-        stations = []
-        with open(self.file_path, "r", encoding="utf-8") as json_file:
-            stations = json.load(json_file)
+        counter = 0
+        for data in self.client.stations():
+            if offset and counter < offset:
+                continue
 
-        if offset and limit and offset < len(stations):
-            stations = stations[offset:limit]
-        elif offset and offset >= len(stations):
-            os.remove(self.file_path)
-            self.need_update = True
-            stations = []
-
-        for data in stations:
             item = CollectionData(
                 name=data.get("name", "").replace("\t", "").replace("\n", "").strip(),
                 url=data.get("url", ""),
             )
             item.add_info(data.get("country"), data.get("countrycode"), str(data.get("bitrate", 0)), data.get("codec"))
             results.append(item)
+
+            counter += 1
+            if limit and len(results) == limit:
+                break
 
         return results
 
