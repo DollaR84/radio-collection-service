15738429505cf877b8a1efa8ae582e18ce21631e
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2025-09-06 13:59:11 +0300

    fixed backups scripts

--- backup/restore.sh
+++ backup/restore.sh
@@ -1,1 +1,22 @@
-cat backup/backup.sql | docker compose exec db pg_dump -U postgres -d radiocenter
+#!/bin/bash
+set -e
+
+DB_NAME="radiocenter"
+DB_USER="postgres"
+
+if [ -n "$1" ]; then
+  BACKUP_FILE="$1"
+else
+  BACKUP_FILE=$(ls -t ./backup/backup_*.sql | head -n 1)
+fi
+
+if [ ! -f "$BACKUP_FILE" ]; then
+  echo "Backup file not found: $BACKUP_FILE"
+  exit 1
+fi
+
+echo "Restoring the database $DB_NAME from file $BACKUP_FILE..."
+
+docker compose exec -T db psql -U "$DB_USER" -d "$DB_NAME" < "$BACKUP_FILE"
+
+echo "Restoration completed!"
